8302baaf0fa7f4e8e9ce05daa33e6e9e8c935efb
Nikhil Shinde <160251430+nikkkhilllit@users.noreply.github.com>
2024-12-17 17:22:29 +0530

    last

--- app_backend/index.js
+++ app_backend/index.js
@@ -15,7 +15,7 @@ const userRoutes = require("./routes/user.js");
 require("dotenv").config();
 const cors = require("cors");
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
